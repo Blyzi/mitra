5e6e80ded68574d37a62ee0cedd5e6f485534c82
Blyzi <flash.spaces@gmail.com>
2025-11-25 02:45:08 +0100

    feat: added Qwen/Qwen3 to get_model func

--- src/utils/get_model.py
+++ src/utils/get_model.py
@@ -1,9 +1,12 @@
 from src.utils.gemma3 import Gemma3Model
+from src.utils.qwen3 import Qwen3Model
 from src.utils.models import Model
 
 
 def get_model(name: str) -> Model:
     if "google/gemma-3" in name:
         return Gemma3Model(name)
+    if "Qwen/Qwen3" in name:
+        return Qwen3Model(name)
 
     raise NotImplementedError(f"Model {name} not implemented")
