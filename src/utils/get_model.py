36d9882b48f89f2bfc6494f550540fb5814585d5
Blyzi <flash.spaces@gmail.com>
2025-11-29 23:43:28 +0100

    feat: added llama3 models

--- src/utils/get_model.py
+++ src/utils/get_model.py
@@ -1,6 +1,7 @@
 from src.utils.gemma3 import Gemma3Model
 from src.utils.qwen3 import Qwen3Model
 from src.utils.models import Model
+from src.utils.llama3 import Llama3Model
 
 
 def get_model(name: str) -> Model:
@@ -8,5 +9,7 @@ def get_model(name: str) -> Model:
         return Gemma3Model(name)
     if "Qwen/Qwen3" in name:
         return Qwen3Model(name)
+    if "meta-llama/Llama-3" in name:
+        return Llama3Model(name)
 
     raise NotImplementedError(f"Model {name} not implemented")
---
+++ src/utils/llama3.py
@@ -0,0 +1,14 @@
+from src.utils.models import Model
+
+
+class Llama3Model(Model):
+    def __init__(self, name):
+        super().__init__(
+            name=name,
+            get_layers_func=lambda llm: llm.model.layers,
+            get_out_proj_func=lambda self_attn: self_attn.o_proj,
+            get_self_attn_func=lambda layer: layer.self_attn,
+            get_n_head_func=lambda llm: llm.config.num_attention_heads,
+            get_d_head_func=lambda llm: llm.config.head_dim,
+            get_d_llm_func=lambda llm: llm.config.hidden_size,
+        )
